277acf0ee02b999d954c5b97db08c5a4894e72b4
hell-rh <helena.raymond-hayling@bbc.co.uk>
2025-06-18 15:31:25 +0100

    inline comments

--- pv.py
+++ pv.py
@@ -5,32 +5,37 @@ import ntcore
 import logging
 import time
 import random
+
+# Set up logging to display debug messages
 logging.basicConfig(level=logging.DEBUG)
 
 # Create a NetworkTables instance and start a client
 inst = ntcore.NetworkTableInstance.getDefault()
-inst.startClient4("photonvision-client")  # start NT4 client with a name
-inst.setServer("127.0.0.1", ntcore.NetworkTableInstance.kDefaultPort4)
+inst.startClient4("photonvision-client")  # Start NT4 client with a name
+inst.setServer("127.0.0.1", ntcore.NetworkTableInstance.kDefaultPort4)  # Set server address and port
 
-camera_name = 'USB_Capture_SDI' #'VirtualBox_Webcam_-_OBS_Virtual_Camera'
+camera_name = 'USB_Capture_SDI'  # Name of the camera to use
 
-# Initialise camera
+# Initialise the PhotonCamera object with the specified camera name
 camera = PhotonCamera(camera_name)
-time.sleep(5)
+time.sleep(5)  # Wait for the camera to initialize
 
-
+# Define the transform from the robot to the camera (position and orientation)
 kRobotToCam = wpimath.geometry.Transform3d(
-    wpimath.geometry.Translation3d(0.5, 0.0, 0.5),
-    wpimath.geometry.Rotation3d.fromDegrees(0.0, -30.0, 0.0),
+    wpimath.geometry.Translation3d(0.5, 0.0, 0.5),  # x, y, z translation in meters
+    wpimath.geometry.Rotation3d.fromDegrees(0.0, -30.0, 0.0),  # roll, pitch, yaw in degrees
 )
 
-
+# Create a PhotonPoseEstimator for estimating the robot's pose using AprilTags
 camPoseEst = PhotonPoseEstimator(
-    AprilTagFieldLayout.loadField(AprilTagField.kDefaultField),
-    PoseStrategy.LOWEST_AMBIGUITY,
-    camera,
-    kRobotToCam,
+    AprilTagFieldLayout.loadField(AprilTagField.kDefaultField),  # Load default AprilTag field layout
+    PoseStrategy.LOWEST_AMBIGUITY,  # Use pose strategy with lowest ambiguity
+    camera,                        # The camera object
+    kRobotToCam,                   # The transform from robot to camera
 )
 
+# Attempt to update the pose estimation from the camera
 camEstPose = camPoseEst.update()
+
+# Print the estimated pose (may be None if no pose is found)
 print(camEstPose.estimatedPose)
